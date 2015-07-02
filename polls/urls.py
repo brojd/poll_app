c8556d2345a425540387d1c2ad11ae54a3b9e996
dominik791 <dominik.broj@gmail.com>
2015-07-02 20:42:42 +0200

    Write more views that actually do something

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'poll_app.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', views.index, name="index"),
+        url(r'^$', views.index, name="index"),
+        url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+        url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+        url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
 ]
